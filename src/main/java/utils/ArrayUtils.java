ac338c8df7fb01fe50fd6474cd2e7da36d950558
DonMarioComputing <omeshmariodon@gmail.com>
2025-09-25 01:40:57 +0100

    "feat: add displayArray method for integer arrays" "docs: add Javadoc for displayArray"

--- src/main/java/utils/ArrayUtils.java
+++ src/main/java/utils/ArrayUtils.java
@@ -1,4 +1,22 @@
 package utils;
 
 public class ArrayUtils {
+
+//Exercise 1.1) Displaying all elements in a numeric array
+
+    /**
+     * Displays all elements in an integer array along with their positions.
+     *
+     * @param arr the integer array to display
+     */
+    public static void displayArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index " + i + ": " + arr[i]);
+        }
+    }
+
 }
+
+
+
+
